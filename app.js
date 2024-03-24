27a66b07f32810b78cfa52ae3dbd1489ebafe9b9
VIHANGA2002 <150515879+VIHANGA2002@users.noreply.github.com>
2024-03-25 04:46:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,31 +12,29 @@ const brandRouter = require("./routes/BrandRoutes");
 const drugRouter = require("./routes/DrugRoutes");
 const productRouter = require("./routes/ProductRoutes");
 const prescriptionRouter = require("./routes/PrescriptionRoutes");
-
+const billRouter = require("./routes/BillRoutes");
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+  origin: "http://localhost:5173",
 };
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
+//databse config
 connectDB();
-
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
 
 app.use("/api/users", userRouter);
 app.use("/api/brands", brandRouter);
 app.use("/api/drugs", drugRouter);
 app.use("/api/products", productRouter);
 app.use("/api/prescription", prescriptionRouter);
+app.use("/api/bills", billRouter);
+
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
-  
