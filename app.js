6be5a21097fb69ea8bad2c7f3366139cdccebf9f
VIHANGA2002 <150515879+VIHANGA2002@users.noreply.github.com>
2024-03-24 12:16:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,13 +7,34 @@ const cors = require("cors");
 const app = express();
 const port = 8000;
 
+const userRouter = require("./routes/UserRoutes");
+const brandRouter = require("./routes/BrandRoutes");
+const drugRouter = require("./routes/DrugRoutes");
+const productRouter = require("./routes/ProductRoutes");
+const prescriptionRouter = require("./routes/PrescriptionRoutes");
+
 
 const corsOptions = {
     origin: "http://localhost:5173",
 };
-  
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
 connectDB();
 
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.use("/api/users", userRouter);
+app.use("/api/brands", brandRouter);
+app.use("/api/drugs", drugRouter);
+app.use("/api/products", productRouter);
+app.use("/api/prescription", prescriptionRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
