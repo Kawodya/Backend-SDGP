26c5321040ebf97386f2db2440063bdc58d7d7ab
Sunera2002 <143516854+Sunera2002@users.noreply.github.com>
2024-03-24 17:28:10 +0530

    Create PrescriptionRoutes.js

---
+++ routes/PrescriptionRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  createPrescription,
+  getAllPrescriptions,
+  addPrescriptionToUser,
+  getPrescriptionByCode,
+  getPrescriptionByUser,
+  getPharmacistsWithProducts,
+} = require("../controllers/PrescriptionController");
+
+router.route("/").get(getAllPrescriptions).post(createPrescription);
+router.route("/add-prescription-to-user").post(addPrescriptionToUser);
+router.route("/get-prescription-by-user/:id").get(getPrescriptionByUser);
+router.route("/get-prescription-by-code/:code").get(getPrescriptionByCode);
+router
+  .route("/get-pharmacists-by-prescription/:district_id/:prescription_id")
+  .get(getPharmacistsWithProducts);
+
+module.exports = router;
