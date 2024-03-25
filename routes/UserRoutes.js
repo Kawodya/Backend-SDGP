13453c4caf06103d24fd9a2f0723216c38a093e9
Sunera2002 <143516854+Sunera2002@users.noreply.github.com>
2024-03-25 10:33:53 +0530

    Update UserRoutes.js

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -9,15 +9,17 @@ const {
   getUserById,
   getAllPharmacist,
   completeRegistration,
+  resetPassword,
 } = require("../controllers/UserController");
 const router = express.Router();
 
+router.route("/reset-password").post(resetPassword);
 router.route("/").get(getAllUsers).post(createUser);
 router.route("/user-verifired").post(completeRegistration);
-router.route("/:userId").get(getUserById);
 router.route("/login").post(loginUser);
 router.route("/doctors").get(getAllDoctors);
 router.route("/patients").get(getAllPatients);
 router.route("/pharmacists").get(getAllPharmacist);
+router.route("/:userId").get(getUserById);
 
 module.exports = router;
